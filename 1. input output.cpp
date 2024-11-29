77e104e46cf763c3e1ba34d5a7899eac6cec0da7
jashwanth06g <115545748+jashwanth06g@users.noreply.github.com>
2024-11-29 22:05:36 +0530

    Add files via upload

---
+++ 1. input output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int i;
+    string s;
+    char c;
+    cin>>i;
+    cin>>s;
+    cin>>c;
+    
+    cout<<i<<endl;
+    cout<<s<<endl;
+    cout<<c<<endl;
+
+    return 0;
+}
