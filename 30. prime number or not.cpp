02e4e638bfd7e0cffb144878dcfaa805a413605f
jashwanth06g <115545748+jashwanth06g@users.noreply.github.com>
2024-12-17 18:28:13 +0530

    Add files via upload

---
+++ 30. prime number or not.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 7, flag = 0;
+    for(int i = 2; i < n/2; i++){
+        if(n%i == 0){
+            flag = 1;
+        }
+    }
+    if(flag == 0){
+        cout<<"Prime number";
+    }
+    else{
+        cout<<"Not a Prime number";
+    }
+    return 0;
+    
+}
